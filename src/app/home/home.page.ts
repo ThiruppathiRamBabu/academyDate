86893537ed99d81d03ecb4717b8f30de471e6b2b
ThiruppathiRamBabu <snthiru2000@gmail.com>
2023-08-17 14:02:35 +0530

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  modes = ['date', 'date-time', 'month', 'month-year', 'time', 'time-date', 'year'];
+  selectMode = 'date';
+  showPicker = false;
+  formattedString = '';
+  @ViewChild(IonDatetime) datetime!: IonDatetime
 
-  constructor() {}
+  dateValue = format(new Date(), 'yyyy-MM-dd') + 'T09:00:00.000Z';
+  constructor() {
+    this.setToday();
+
+   }
+
+  setToday(){
+    this.formattedString = format(parseISO(this.dateValue), 'HH:mm, MMM d, yyyy');
+    };
+dateChanged(value: any){
+  this.dateValue = value;
+  this.formattedString = format(parseISO(value), 'HH:mm , MMM d, yyyy');
+  this.showPicker = false;
+}
+
+close(){
+this.datetime.cancel(true);
+}
+select(){
+this.datetime.confirm(true);
+}
 
 }
